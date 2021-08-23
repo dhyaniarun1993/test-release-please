6c9ac9f3db65e9d29d7ce66a09ac54ab221ccfea
Arun Dhyani <dhyaniarun7@gmail.com>
2021-08-23 10:58:45 +0530

    feat: status handler added

--- main.go
+++ main.go
@@ -9,8 +9,13 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello\n")
 }
 
+func status(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "healthy\n")
+}
+
 func main() {
 	fmt.Println("Starting Test Release Please Server")
 	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/status", status)
 	http.ListenAndServe(":8090", nil)
 }
